a0285164ff67b50a15224becc994d59cb56239d1
XiChen1995 <xc129@scarletmail.rutgers.edu>
2018-05-23 00:17:42 -0400

    added images and tab bar

---
+++ pages/index/index.js
@@ -0,0 +1,15 @@
+//index.js
+
+const app = getApp()
+
+Page({
+  data: {
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    circular: true,
+    indicatorDots: true,
+    indicatorcolor: "#000",
+    autoplay: true,
+    interval: 2500,
+    duration: 1000,
+  }
+})
