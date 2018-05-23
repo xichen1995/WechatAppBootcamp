cd7277728b588e7f3b09a448db92b4e5013eb294
Lucas You <lizhao.you@stonybrook.edu>
2018-05-22 23:53:22 -0400

    menu & items

--- WechatApp/pages/index/index.js
+++ WechatApp/pages/index/index.js
@@ -4,12 +4,13 @@ const app = getApp()
 
 Page({
   data: {
+    //swiper
     background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
     circular: true,
     indicatorDots: true,
     indicatorcolor: "#000",
     autoplay: true,
     interval: 2500,
-    duration: 1000,
-  }
+    duration: 1000
+  },
 })
